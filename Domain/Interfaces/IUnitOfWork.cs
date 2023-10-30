7a0f37ad031be0bcd4211b61e561510a8425030b
Lenin2611 <leninsantiago2611@gmail.com>
2023-10-30 10:29:39 -0500

    feat: :sparkles: Created unitOfWork

---
+++ Domain/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces;
+
+public interface IUnitOfWork
+{
+    public ICiudadRepository Ciudades { get; }
+    public IContactoPersonaRepository ContactoPersonas { get; }
+    public IDepartamentoRepository Departamentos { get; }
+    public IDetalleMovimientoInventarioRepository DetalleMovimientoInventarios { get; }
+    public IFacturaRepository Facturas { get; }
+    public IFormaPagoRepository FormaPagos { get; }
+    public IInventarioRepository Inventarios { get; }
+    public IMarcaRepository Marcas { get; }
+    public IMovimientoInventarioRepository MovimientoInventarios { get; }
+    public IPaisRepository Paises { get; }
+    public IPersonaRepository Personas { get; }
+    public IPresentacionRepository Presentaciones { get; }
+    public IProductoRepository Productos { get; }
+    public IRolPersonaRepository RolPersonas { get; }
+    public ITipoContactoRepository TipoContactos { get; }
+    public ITipoDocumentoRepository TipoDocumentos { get; }
+    public ITipoMovimientoInventarioRepository TipoMovimientoInventarios { get; }
+    public ITipoPersonaRepository TipoPersonas { get; }
+    public IUbicacionPersonaRepository UbicacionPersonas { get; }
+    public IUserRepository Users { get; }
+    public IRolRepository Rols { get; }
+    public IRefreshTokenRepository RefreshTokens { get; }
+
+    Task<int> SaveAsync();
+}
